1ccc757b61945442bad3f48ba87a655c2fa9a053
Rian Anugrah <rian.te.quiero@gmail.com>
2021-10-06 17:31:49 +0700

    Update Article List

--- components/ArticleList.js
+++ components/ArticleList.js
@@ -5,7 +5,7 @@ const ArticleList = ({ articles }) => {
   return (
     <div className={ArticleStyles.grid}>
       {articles.map((article) => (
-        <ArticleItem article={article} />
+        <ArticleItem article={article} key={article.id}/>
       ))}
     </div>
   );
