bbe93c71890f0ffdc16973cfc8de6c305ff6bc37
Rian Anugrah <rian.te.quiero@gmail.com>
2021-10-11 10:34:00 +0700

    Fix NextJs Build

--- pages/profile/[id]/index.js
+++ pages/profile/[id]/index.js
@@ -1,22 +1,19 @@
 import React from "react";
 import { Layout } from "../../../lib";
-import AboutMe from "./AboutMe";
-import ServicesList from "./ServicesList";
-import Testimony from "./Testimony";
+import AboutMe from "../../../lib/profiles/AboutMe";
+import ServicesList from "../../../lib/profiles/ServicesList";
+import Testimony from "../../../lib/profiles/Testimony";
 const profileDetail = ({ profile }) => {
-  console.log(profile);
-  console.log("node js only", process.env.API_HOST);
-
   return (
     <Layout>
-      <AboutMe profile={profile} />
-      <ServicesList services={profile.services} />
-      <Testimony services={profile.services} />
+      <AboutMe profile={profile ? profile : []} />
+      <ServicesList services={profile.services ? profile.services : []} />
+      <Testimony services={profile.services ? profile.services : []} />
     </Layout>
   );
 };
 
-export const getServerSideProps = async (context) => {
+export const getStaticProps = async (context) => {
   const res = await fetch(
     `${process.env.API_HOST}/profiles/${context.params.id}`
   );
@@ -27,6 +24,22 @@ export const getServerSideProps = async (context) => {
     props: {
       profile,
     },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const res = await fetch(`${process.env.API_HOST}/profiles/`);
+
+  const profiles = await res.json();
+
+  const ids = profiles.map((profile) => profile.id);
+
+  const paths = ids.map((id) => ({
+    params: { id: id.toString() },
+  }));
+  return {
+    paths,
+    fallback: false,
   };
 };
 
