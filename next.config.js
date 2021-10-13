4954d550d3e6fe48996839c278e1645b03c21e04
Rian Anugrah <rian.te.quiero@gmail.com>
2021-10-13 14:55:59 +0700

    fix next config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   reactStrictMode: true,
-}
+
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    mySecret: "secret",
+    api_url: process.env.API_HOST,
+    secondSecret: process.env.SECOND_SECRET, // Pass through env variables
+  },
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    staticFolder: "/static",
+    api_url: process.env.API_HOST,
+  },
+};
